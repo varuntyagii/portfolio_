63023be2d3e085725a0e3bfe7c935bf48491f403
Varun Tyagi <varuntyagi0099@gmail.com>
2026-08-30 01:39:31 +0530

    fix home scrolling and blend mode

--- p1/src/App.jsx
+++ p1/src/App.jsx
@@ -1,32 +1,29 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Lenis from 'lenis'
+import React, { useContext, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Lenis from "lenis";
 
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import About from './pages/About'
-import Navbar from './components/navbar/Navbar'
-import LoadingPage from './components/common/LoadingPage'
-import FullScreenNav from './components/navbar/FullScreenNav'
-import ProjectDetail from './components/projects/ProjectDetail'
-import Aboutme from './about/Aboutme'
-import Cursor from './components/common/Cursor'
-import Music from './components/home/Music'
-import Experience from './about/Experience'
-import Contact from './components/common/Contact'
-import Privacy from './components/privacyandpolicy/Privacy'
-import TermsOfService from './components/privacyandpolicy/TermsOfService'
-import TargetCursor from './components/common/TargetCursor'
-import { NavHoverContext } from './context/MenuContext'
-import NotFound from './components/common/NotFound'
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Navbar from "./components/navbar/Navbar";
+import LoadingPage from "./components/common/LoadingPage";
+import FullScreenNav from "./components/navbar/FullScreenNav";
+import ProjectDetail from "./components/projects/ProjectDetail";
+import Aboutme from "./about/Aboutme";
+import Music from "./components/home/Music";
+import Contact from "./components/common/Contact";
+import Privacy from "./components/privacyandpolicy/Privacy";
+import TermsOfService from "./components/privacyandpolicy/TermsOfService";
+import TargetCursor from "./components/common/TargetCursor";
+import { NavHoverContext } from "./context/MenuContext";
+import NotFound from "./components/common/NotFound";
 
 const App = () => {
-  const [menu, setMenu] = useState(false);
-  const { navHovered, setNavHovered } = useContext(NavHoverContext);
+
+  const { navHovered } = useContext(NavHoverContext);
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
       smoothWheel: true,
       lerp: 0.1,
     });
@@ -44,38 +41,38 @@ const App = () => {
   }, []);
 
   return (
-    <div className='overflow-x-clip'>    
-      <div>
-        {!navHovered && (
-          <TargetCursor
-            spinDuration={2}
-            hideDefaultCursor
-            parallaxOn
-            hoverDuration={0.2}
-            cursorColor="#ffffff"
-            cursorColorOnTarget="white"
-          />
-        )}
+    <div className="overflow-x-clip">
 
-        <Navbar />
-        <FullScreenNav />
-        <Music />
-      </div>
+      {!navHovered && (
+        <TargetCursor
+          spinDuration={2}
+          hideDefaultCursor
+          parallaxOn
+          hoverDuration={0.2}
+          cursorColor="#ffffff"
+          cursorColorOnTarget="white"
+        />
+      )}
+
+      <Navbar />
+      <FullScreenNav />
+      <Music />
 
       <Routes>
         <Route path="*" element={<NotFound />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/aboutme' element={<Aboutme />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/aboutme" element={<Aboutme />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
-        <Route path='/loading' element={<LoadingPage />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/privacy-policy' element={<Privacy />} />
-        <Route path='/terms' element={<TermsOfService />} />
+        <Route path="/loading" element={<LoadingPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy-policy" element={<Privacy />} />
+        <Route path="/terms" element={<TermsOfService />} />
       </Routes>
-    </div>
-  )
-}
 
-export default App
+    </div>
+  );
+};
+
+export default App;
--- p1/src/main.jsx
+++ p1/src/main.jsx
@@ -1,11 +1,10 @@
-import { Children, StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Stair from './components/common/Stair'
 import { MenuProvider } from './context/MenuContext'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
