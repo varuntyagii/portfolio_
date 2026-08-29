b6ecd75263d42f24f4f12b9ccc81ab9b0548f73f
Varun Tyagi <varuntyagi0099@gmail.com>
2026-08-30 00:56:56 +0530

    add smooth scrolling with lenis

--- p1/src/App.jsx
+++ p1/src/App.jsx
@@ -1,5 +1,7 @@
-import React, { useContext, useRef, useState } from 'react'
-import {  Route, Routes } from 'react-router-dom'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Lenis from 'lenis'
+
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import About from './pages/About'
@@ -18,50 +20,59 @@ import TargetCursor from './components/common/TargetCursor'
 import { NavHoverContext } from './context/MenuContext'
 import NotFound from './components/common/NotFound'
 
-
 const App = () => {
-const [menu, setMenu] = useState(false);
-  const {navHovered, setNavHovered } = useContext(NavHoverContext)
+  const [menu, setMenu] = useState(false);
+  const { navHovered, setNavHovered } = useContext(NavHoverContext);
 
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      lerp: 0.1,
+    });
 
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
 
   return (
-<div className='overflow-x-clip'>    
-  <div>
+    <div className='overflow-x-clip'>    
+      <div>
+        {!navHovered && (
+          <TargetCursor
+            spinDuration={2}
+            hideDefaultCursor
+            parallaxOn
+            hoverDuration={0.2}
+            cursorColor="#ffffff"
+            cursorColorOnTarget="white"
+          />
+        )}
 
- {/* <Cursor /> */}
- {/* <PixelTrail /> */}
- {!navHovered && (
-  <TargetCursor
-    spinDuration={2}
-    hideDefaultCursor
-    parallaxOn
-    hoverDuration={0.2}
-    cursorColor="#ffffff"
-    cursorColorOnTarget="white"
-  />
-)}
+        <Navbar />
+        <FullScreenNav />
+        <Music />
+      </div>
 
-
-
-<Navbar />
-<FullScreenNav />
- <Music/>
-
-  </div >
       <Routes>
-          <Route path="*" element={<NotFound />} />
-        <Route path='/' element={<Home/>}/>   
-        <Route path='/about' element={<About/>}/>   
-        <Route path='/projects' element={<Projects/>}/>   
-        <Route path='/aboutme' element={<Aboutme/>}/>   
+        <Route path="*" element={<NotFound />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/aboutme' element={<Aboutme />} />
         <Route path="/projects/:id" element={<ProjectDetail />} />
-        <Route path='/loading' element={<LoadingPage/>}/>   
-        <Route path='/contact' element={<Contact/>}/>   
-        <Route path='/privacy-policy' element={<Privacy/>}/>   
-        <Route path='/terms' element={<TermsOfService/>}/>   
-        {/* <Route path='/exp' element={<Experience/>}/>    */}
-       
+        <Route path='/loading' element={<LoadingPage />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/privacy-policy' element={<Privacy />} />
+        <Route path='/terms' element={<TermsOfService />} />
       </Routes>
     </div>
   )
--- p1/src/main.jsx
+++ p1/src/main.jsx
@@ -5,20 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Stair from './components/common/Stair'
 import { MenuProvider } from './context/MenuContext'
-import Lenis from "lenis";
-import "lenis/dist/lenis.css";
 
-const lenis = new Lenis({
-  duration: 1.2,
-  smoothWheel: true,
-});
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
